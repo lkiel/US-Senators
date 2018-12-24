9c595e23fbd7259f7d434914ce9501c3ae6d2ac6
lkieliger <leandro.kieliger@epfl.ch>
2018-12-24 21:25:26 +0100

    Implement graph embedding and improve graph visualisation

--- helpers.py
+++ helpers.py
@@ -2,15 +2,40 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+def plot_adjacency(adjacency):
+    plt.figure(figsize=(9,9))
+    plt.spy(adjacency, markersize=3)
+    plt.title('adjacency matrix')
+    plt.show()
+
+
 def cosine_similarity(x,y):
     """
-    Given two vectors of the same length, return the cosine similarity betweem the two vectors.
+    Given two vectors of the same length, return the cosine similarity between the two vectors.
     """
     return 0.5 * (1 + (np.dot(x,y))/(np.linalg.norm(x)*np.linalg.norm(y)))
-	
+    
+    
+def gaussian_kernel(distance_matrix):
+    """
+    Given a matrix calculate the gaussian kernel of it. The variance is given by the mean of the matrix. 
+    """
+    kernel_width = distance_matrix.mean()
+    res = np.exp(-distance_matrix**2 / kernel_width**2)
+    return res
+    
+
+def euclidean_distance(x,y):
+    """
+    Given two vectors of the same length, return the euclidean distance between the two vectors.
+    """
+    return np.sqrt(np.sum(np.power(x-y,2)))
+    
+    
 def get_adjacency_multidistance(features_list, weights, distance_function, kernel_function, sparsify):
     '''
-    Construct an adjecency matrix matrix using  the features passed in parameter.
+    Construct an adjacency matrix matrix using  the features passed in parameter.
     The final result is computed as a weighted sum of the distances matrices obtained using the different features 
     '''
     number_of_nodes = features_list[0].shape[0]
@@ -25,7 +50,7 @@ def get_adjacency_multidistance(features_list, weights, distance_function, kerne
         result += weights[i] * sparsify[i](kernel)
         
     return result  
-	
+    
 def sparsify_with_limit(adjacency, limit = 0.35):
     """
     Sparsify a matrix by putting each of element of the adjacency matrix to 0
@@ -61,38 +86,59 @@ def sparsify_with_max_neighbors(adjacency, max_neighbors = 45):
                    
     return res
 
-def plot_graph(G, node_color, edge_threshold=0.5, scale=None, highlight_node=[], ax=None, colormap=plt.get_cmap('coolwarm')):
-    pos = nx.spring_layout(G,seed=2019, iterations=200)
+def plot_graph(G, node_color, edge_threshold=0.5, scale=None, highlight_node=[], ax=None, colormap=plt.get_cmap('Set1'), positions=None):
     
-    elarge = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] > edge_threshold]
-    esmall = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] <= edge_threshold]
-
-    nx.draw_networkx_edges(G, pos, edgelist=esmall,
-                       width=0.5, alpha=0.5, ax=ax)
-    nx.draw_networkx_edges(G, pos, edgelist=elarge,
-                       width=1, alpha=0.5, ax=ax)
+    pos = nx.spring_layout(G,seed=2018, iterations=400, pos=positions, k=1.5)
+    
+    e_weights = nx.get_edge_attributes(G,'weight')
+    e_weights = np.array(list(e_weights.values()))
+    e_weights = 0.5 + 2 * (e_weights - np.min(e_weights))/(np.max(e_weights) - np.min(e_weights))
+    
+    nx.draw_networkx_edges(G, pos, width=e_weights, alpha=0.2, ax=ax, style='dotted')
+    
     
     if scale is not None:
         vmin = scale[0]
         vmax = scale[1]
     else:
         vmin= None
-        vmax=None
+        vmax= None
         
-    if len(highlight_node)>0:
-        node_size=[900 if x in highlight_node else 300  for x in G.nodes()]
-    else:
-        node_size = 300
-    nc = nx.draw_networkx_nodes(G,pos,with_labels=True,node_color= node_color, cmap=colormap,
-                               vmin=vmin,vmax=vmax,node_size=node_size,ax=ax)
-    nx.draw_networkx_labels(G, pos, alpha=0.5,ax=ax)
+    node_size = [2000 if i in highlight_node else 300  for i,_ in enumerate(G.nodes())]
+    node_color = np.array([0.6 if i in highlight_node else node_color[i] for i,_ in enumerate(G.nodes())])
+    
+    
+    normal_nodes = [n for i,n in enumerate(G.nodes()) if i not in highlight_node]
+    normal_nodes_indices = [i for i,n in enumerate(G.nodes()) if i not in highlight_node]
+    special_nodes = [n for i,n in enumerate(G.nodes()) if i in highlight_node]
+    
+    print(special_nodes)
+    
+    
+    nc = nx.draw_networkx_nodes(G,pos,with_labels=True, nodelist=normal_nodes, node_color=node_color[normal_nodes_indices], cmap=colormap,
+                               vmin=vmin,vmax=vmax,node_size=300,ax=ax, edgecolors='black', alpha=0.6)
+                               
+    nx.draw_networkx_labels(G, pos, alpha=0.7, color='gray', ax=ax)
+                            
+    nc = nx.draw_networkx_nodes(G,pos,with_labels=True, nodelist=special_nodes, node_color=[0.6], cmap=colormap,
+                               vmin=vmin,vmax=vmax,node_size=2000,ax=ax, edgecolors='black', alpha=1.0, node_shape='H')
+                               
+    
+    
+    
     plt.axis('off')
+    
     if ax is not None:
         ax.axis('off')
+        
     return nc
-	
-	
-def plot_signal(adjacency, signal, **kwargs):
+    
+    
+def plot_signal(adjacency, signal, labels=None, **kwargs):
     adjacency_temp = adjacency.copy()
     G = nx.from_numpy_matrix(adjacency_temp)    
+    
+    if labels is not None:
+        G = nx.relabel_nodes(G, lambda x : labels[x])
+    
     return plot_graph(G,np.round(signal,5), edge_threshold = np.max(adjacency) * 0.9, **kwargs)
